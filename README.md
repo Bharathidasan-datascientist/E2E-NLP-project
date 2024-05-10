b67dc6c7428d568f811bafdeae0631b2b61aa483
Bharathi <bharathidasan.cse@gmail.com>
2024-05-11 04:28:04 +0530

    requirements_added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # E2E-NLP-project
+
+##How to run?
+
+```bash
+conda create -n hate python==3.11 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Bharathi",
+    author_email="bharathidasan.cse@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
