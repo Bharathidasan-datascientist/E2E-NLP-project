c92e0c8ee49d1a36a58b6162a187b6f8b017507f
Bharathi <bharathidasan.cse@gmail.com>
2024-05-13 02:05:05 +0530

    all_updated

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ pip install -r requirements.txt
 
 #Gcloud cli
 https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
+
+```bash
+gcloud init
+```
--- demo.py
+++ demo.py
@@ -1,11 +1,8 @@
 from hate.logger import logging
 from hate.exception import CustomException
 import sys
+from hate.configuration.gcloud_syncer import GCloudSync
 
-logging.info("Welcome to our Project")
 
-
-try:
-    a=7/"0"
-except Exception as e:
-    raise CustomException(e,sys) from e 
+obj=GCloudSync()
+obj.sync_folder_from_gcloud("hate-speech-2024-live","dataset.zip","dataset.zip")
